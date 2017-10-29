98dd1b5e6ac9ce7140c30a39f73503156aef3728
Yuqian Yang <yyang@freewheel.tv>
2017-10-29 23:33:21 +0800

    sharedSubnetName

--- pkg/model/network.go
+++ pkg/model/network.go
@@ -146,11 +146,11 @@ func (b *NetworkModelBuilder) Build(c *fi.ModelBuilderContext) error {
 		subnetSpec := &b.Cluster.Spec.Subnets[i]
 		sharedSubnet := subnetSpec.ProviderID != ""
 		subnetName := subnetSpec.Name + "." + b.ClusterName()
-		tags := b.CloudTags(subnetName, sharedSubnet)
-
-		if b.Cluster.Spec.SkipUpdateSubnetName {
-			delete(tags, "Name")
+		if b.Cluster.Spec.SharedSubnetName {
+			subnetName = subnetSpec.Name
 		}
+
+		tags := b.CloudTags(subnetName, sharedSubnet)
 
 		// Apply tags so that Kubernetes knows which subnets should be used for internal/external ELBs
 		switch subnetSpec.Type {
