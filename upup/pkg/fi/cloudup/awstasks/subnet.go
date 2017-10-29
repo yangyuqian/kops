de9c62388b19664877aba569ed4baedb0d8a7db6
Yuqian Yang <yyang@freewheel.tv>
2017-10-29 23:03:49 +0800

    remove dangerous code

--- upup/pkg/fi/cloudup/awstasks/subnet.go
+++ upup/pkg/fi/cloudup/awstasks/subnet.go
@@ -161,7 +161,7 @@ func (_ *Subnet) RenderAWS(t *awsup.AWSAPITarget, a, e, changes *Subnet) error {
 			return fmt.Errorf("Subnet with id %q not found", fi.StringValue(e.ID))
 		}
 
-		return t.AddAWSTags(*e.ID, e.Tags)
+		return nil
 	}
 
 	if a == nil {
